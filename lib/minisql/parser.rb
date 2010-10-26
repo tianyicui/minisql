63230aab95b067e07ccc316e1af341a00e831f96
Tianyi Cui <tianyicui@gmail.com>
2010-10-26 13:47:07 +0800

    splited treetop files

--- lib/minisql/parser.rb
+++ lib/minisql/parser.rb
@@ -1,2 +1,13 @@
 require 'treetop'
-Treetop.load (Pathname(__FILE__).dirname+"parser/grammar.treetop").to_s
+[
+  'keywords',
+  'common',
+  'create_table_exp',
+  'drop_table_exp',
+  'create_index_exp',
+  'drop_index_exp',
+  'select_exp',
+  'grammar'
+].each do |f|
+  Treetop.load (Pathname(__FILE__).dirname+"parser/#{f}").to_s
+end
