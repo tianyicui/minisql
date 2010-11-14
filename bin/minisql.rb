4a631a26c826520c99725ad295dc815ea735460b
Tianyi Cui <tianyicui@gmail.com>
2010-11-14 15:30:47 +0800

    bin

---
+++ bin/minisql.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
+
+require 'minisql'
+require 'readline'
+
+db = MiniSQL::Database.new "./minisql.db"
+
+while line = Readline.readline('> ', true)
+  begin
+    puts db.execute(line)
+  rescue Exception => e
+    puts e
+  end
+end
